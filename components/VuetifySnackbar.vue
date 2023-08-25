b3af1fb1dd572d564f13865f28b7ff0138342782
unknown-o <i@mr-wu.top>
2023-08-25 16:47:50 +0800

    feat: 新增snackbar组件

---
+++ components/VuetifySnackbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="text-center">
+    <v-snackbar v-model="snackbar" :timeout="snackbarTimeout" multi-line>
+      {{ snackbarText }}
+    </v-snackbar>
+  </div>
+</template>
+
+<script lang="ts">
+import { ref } from "vue";
+export default {
+  setup() {
+    let snackbar = ref(false);
+    let snackbarTimeout = ref(0);
+    let snackbarText = ref("test");
+    function show(text: string, timeout = 3000, color = "blue") {
+      snackbar.value = true;
+      snackbarTimeout.value = timeout;
+      snackbarText.value = text;
+      return true;
+    }
+    return { show, snackbar, snackbarTimeout, snackbarText };
+  },
+};
+</script>
