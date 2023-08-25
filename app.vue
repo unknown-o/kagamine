cd78fef98fdf7ad722d12f2caf9eaaedfeac0039
unknown-o <i@mr-wu.top>
2023-08-25 21:31:03 +0800

    feat: 新增跟踪代码

--- app.vue
+++ app.vue
@@ -1,7 +1,12 @@
 <template>
+  <script
+    async
+    src="https://analysis.llilii.cn/script.js"
+    data-website-id="a1f454da-8122-4c78-9125-33823e28f225"
+  ></script>
   <NuxtPage />
 
-  <p style="margin: 40px; text-align: center;">
+  <p style="margin: 40px; text-align: center">
     <a
       style="color: black"
       href="https://beian.miit.gov.cn/"
