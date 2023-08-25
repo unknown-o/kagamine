ee8923f2504c56ebafa015e6be8f51d69d4e1267
unknown-o <i@mr-wu.top>
2023-08-25 15:08:10 +0800

    feat: 新增页脚备案

--- app.vue
+++ app.vue
@@ -1,3 +1,23 @@
 <template>
-    <NuxtPage />
+  <NuxtPage />
+
+  <p style="margin: 40px; text-align: center">
+    <a
+      style="color: black"
+      href="https://beian.miit.gov.cn/"
+      rel="noopener"
+      target="_blank"
+    >
+      浙ICP备2023001984号-5
+    </a>
+    <br />
+    <a
+      style="color: black"
+      href="https://www.beian.gov.cn/portal/registerSystemInfo?recordcode=33108102001067"
+      rel="noopener"
+      target="_blank"
+    >
+      浙公网安备 33108102001067号
+    </a>
+  </p>
 </template>
