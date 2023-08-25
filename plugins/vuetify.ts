6dbe7c92c8b06f70eef84d5fd098cebe98d6f902
unknown-o <i@mr-wu.top>
2023-08-25 13:37:56 +0800

    mod: 更新vuetify主题颜色

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -2,9 +2,30 @@ import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 
 export default defineNuxtPlugin((nuxtApp) => {
-  const vuetify = createVuetify({
-    components,
-  });
+    const vuetify = createVuetify({
+        components,
+        theme: {
+            defaultTheme: 'light',
+            themes: {
+                light: {
+                    dark: false,
+                    colors: {
+                        primary: "#ffe211",
+                        second: "#ffa500",
+                        accent: "#d81b60",
+                    }
+                },
+                dark: {
+                    dark: true,
+                    colors: {
+                        primary: "#ffe211",
+                        second: "#ffa500",
+                        accent: "#d81b60",
+                    }
+                },
+            }
+        }
+    });
 
-  nuxtApp.vueApp.use(vuetify);
+    nuxtApp.vueApp.use(vuetify);
 });
