342ba26fc9def4509dc81172c5f57020c038f865
unknown-o <i@mr-wu.top>
2023-08-25 15:57:48 +0800

    feat: 新增留言墙页面

--- pages/messages.vue
+++ pages/messages.vue
@@ -1,4 +1,172 @@
 <template>
-  <title>留言墙 - 镜音的小窝</title>
-  This page is under development
+  <title>MessageBoard - KagamineFans</title>
+  <v-container>
+    <v-row dense>
+      <v-col cols="12" xs="12" sm="6" md="5" lg="5" offset-lg="1">
+        <h1 class="my-6 ml-1 justify text-primary">
+          <router-link to="/">
+            <v-btn class="text-yellow-accent-4" variant="text">
+              <v-icon size="25">mdi-arrow-left</v-icon>
+            </v-btn>
+          </router-link>
+          MessageBoard
+        </h1>
+      </v-col>
+    </v-row>
+
+    <v-row dense>
+      <v-col cols="12" xs="12" sm="4" lg="3" offset-lg="1">
+        <v-hover v-slot="{ isHovering, props }">
+          <v-card :elevation="isHovering ? 12 : 1" v-bind="props" class="mb-2">
+            <v-card-text>
+              <div class="ma-3 justify">
+                <h2>提交留言</h2>
+                <v-divider class="my-4 justify"></v-divider>
+                <v-text-field
+                  label="Your Nickname..."
+                  v-model="submitForm.nickname"
+                  required
+                ></v-text-field>
+                <v-text-field
+                  label="Your E-mail..."
+                  v-model="submitForm.email"
+                  required
+                ></v-text-field>
+                <v-textarea
+                  outlined
+                  name="input-7-4"
+                  v-model="submitForm.message"
+                  label="You Want To Say..."
+                >
+                </v-textarea>
+              </div>
+            </v-card-text>
+            <v-card-actions class="justify-end">
+              <v-btn class="ma-3" @click="send">发射！</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-hover>
+
+        <v-hover v-slot="{ isHovering, props }">
+          <v-card :elevation="isHovering ? 12 : 1" v-bind="props" class="mb-2">
+            <v-card-text>
+              <div class="ma-3 justify">
+                <h2>显示设置</h2>
+                <v-divider class="my-4 justify"></v-divider>
+                <v-select
+                  v-model="sortValue"
+                  :items="sortText"
+                  label="排序方式"
+                ></v-select>
+              </div>
+            </v-card-text>
+          </v-card>
+        </v-hover>
+      </v-col>
+      <v-col cols="12" xs="12" sm="8" lg="7">
+        <v-row
+          v-if="messageList.length == 0"
+          class="fill-height ma-0"
+          align="center"
+          justify="center"
+        >
+          <v-progress-circular
+            :size="50"
+            color="amber"
+            indeterminate
+          ></v-progress-circular>
+        </v-row>
+        <div v-for="item in messageList">
+          <v-hover v-slot="{ isHovering, props }">
+            <v-card :elevation="isHovering ? 12 : 1" v-bind="props" class="mb-2">
+              <v-card-title>{{ item.nickname }}</v-card-title>
+              <v-card-subtitle>{{ formatDate(item.timestamp * 1000) }}</v-card-subtitle>
+              <v-card-text>
+                {{ item.message }}
+              </v-card-text>
+              <v-card-actions class="justify-end">
+                <p v-if="item.status < 1">未经审核的</p>
+                <v-btn
+                  size="small"
+                  :data-message-id="item.id"
+                  @click="like"
+                  class="text-red"
+                  variant="text"
+                  icon="mdi-heart"
+                ></v-btn>
+                <p style="font-size: 1px">{{ item.likes }}</p>
+                <v-btn
+                  size="small"
+                  :data-message-id="item.id"
+                  @click="report"
+                  class="text-yellow-accent-4"
+                  variant="text"
+                  icon="mdi-alert"
+                ></v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-hover>
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+
+<script lang="ts" setup>
+import { requestApi, formatDate } from "../plugins/common";
+import { ref, reactive, watch } from "vue";
+let messageList = ref([]);
+let submitForm = reactive({
+  nickname: "",
+  email: "",
+  message: "",
+});
+let sortValue = ref("Likes");
+const sortText = ref(["Likes", "Send Time", "Random"]);
+const sortItem = { Likes: "likes", "Send Time": "time", Random: "random" };
+watch(
+  () => sortValue.value,
+  (newValue) => {
+    getMessages(sortItem[newValue], 20, 0);
+  }
+);
+
+const getMessages = (sort = "likes", limit = 20, offset = 0, showUnaudited = false) => {
+  messageList.value = [];
+  requestApi(
+    "/messages",
+    false,
+    "get",
+    {
+      sort: sort,
+      limit: limit,
+      offset: offset,
+      show_unaudited: showUnaudited,
+    },
+    {},
+    function (rdata) {
+      if (rdata.status == 200) {
+        messageList.value = rdata.data.data;
+      } else {
+        snackbar.add({
+          type: "error",
+          text: rdata.data.msg,
+          duration: 3000,
+        });
+      }
+    },
+    function (error) {
+      snackbar.add({
+        type: "error",
+        text: error.message,
+        duration: 3000,
+      });
+    }
+  );
+};
+getMessages();
+
+const send = () => {};
+const like = () => {};
+const report = () => {};
+</script>
