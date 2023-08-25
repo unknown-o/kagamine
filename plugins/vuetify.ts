bbfa5c69c611adc2e1bef72c712ca5532a6dbb91
unknown-o <i@mr-wu.top>
2023-08-25 15:50:32 +0800

    chore: 引用mdi图标

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -1,5 +1,6 @@
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
+import '@mdi/font/css/materialdesignicons.css'
 
 export default defineNuxtPlugin((nuxtApp) => {
     const vuetify = createVuetify({
