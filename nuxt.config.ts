825bd731d1604717f8f5a8e3543dcc71bc40600c
unknown-o <yushen@mr-wu.top>
2025-07-20 14:41:55 +0000

    mod: 更新依赖版本

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
   css: ["@/assets/scss/style.scss"],
   build: {
     // transpile 选项中可以配置需要转译的文件或模块的名称，这些文件或模块会在编译过程中被转译，以保证其兼容性。
-    transpile: ["vuetify"],
+    transpile: ["vuetify", 'form-data'],
   },
   extends: ['nuxt-umami'],
   appConfig: {
